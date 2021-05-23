48f41060d8d6bd1fe151d2d7d95bf9724258cf85
riri <claraligz@gmail.com>
2021-05-23 21:27:14 +0800

    Modified: generate and store 200 coupon

--- leigitwing/0001/redeem_num.py
+++ leigitwing/0001/redeem_num.py
@@ -1,28 +1,23 @@
-# 为 app 生成优惠码，16位大写字母和数字混合的字符串
+# 为 app 生成 200 个优惠码，16 位大写字母和数字混合的字符串
 __author__ = 'Lei gitwing'
 __create__ = '2021-05-22'
-import string, random
+__modified__ = '2021-05-23'
+import random
 
-def generate_redeem_code():
-    count = 0
-    result = ''
+def generate_redeem_code(num):
+    code_length = 16
     
-    while True:
-        letter = random.choice(string.ascii_uppercase)
-        digit = random.choice(string.digits)
-        # 数字出现次数 0-4 次，出现位置也随机
-        if count == random.choice(range(0, 17)):
-            result += digit
-            count += 1
-        result += letter
-        count += 1
+    file = open('coupon.txt', 'w')
+    chars = 'ABCDEFGHIJKLMNOPGRSTUVWXYZ1234567890'
+    for i in range(num):
+        result = ''
+        for j in range(code_length):
+            result += random.choice(chars)
 
-        if count == 16:
-            break
-        
-    print(result)
-    print(len(result) == 16)  # Check if the length of the result is 16
+        file.write(result + "\n")
+    file.close()    
+
+    # print(len(result) == 16)  # Check if the length of the result is 16
 
 if __name__ == '__main__':
-    generate_redeem_code()
-    
+    generate_redeem_code(200)
